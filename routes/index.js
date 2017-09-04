52fe15a7d5fa2541514861ef000223a1eb187046
Flemming Hoffmeyer <flemming@festinafinance.com>
2017-09-04 22:25:10 +0200

    oh the joy with dates and time

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let moment = require('moment');
+
+let date = new Date();
+let today = moment().format('YYYY-MM-DD');
+let now = moment().format('HH:mm');
+
+let time = {
+  startDate: today
+, startTime: now
+, endDate: today
+, endTime: now 
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(time);
+  res.render('index', time);
 });
 
 module.exports = router;
