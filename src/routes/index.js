1e85132b12296463b75ce7f82285734e2361d455
Flemming Hoffmeyer <flemming.b.h@gmail.com>
2017-10-09 22:22:17 +0200

    more ramda play

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,5 @@
 // @flow
 
-import util from 'util';
-
 import {Router} from 'express';
 import R from 'ramda';
 import moment from 'moment';
@@ -11,21 +9,23 @@ import {getWork} from '../db';
 
 let router = Router();
 
-const getNewWorkHours = (): Work => {
-  return {
-    id: null,
-    start: new Date(),
-    duration: 0,
-  };
+const defaultWork: Work = {
+  id: null,
+  start: new Date(),
+  duration: 0,
 };
 
-const useOrNew = (work: ?Work): Work =>
-  work != null && work.duration == 0 ? work : getNewWorkHours();
+const isInProgress: Work => boolean = R.pipe(R.prop('duration'), R.equals(0));
+
+const inProgressOrNew = R.when(
+  R.either(R.isNil, R.complement(isInProgress)),
+  R.always(defaultWork),
+);
 
 router.get('/', (req, res, next) => {
   const date: string = moment(new Date()).format('YYYY-MM-DD');
   getWork(date)
-    .then(R.compose(workToUiWork, useOrNew))
+    .then(R.pipe(inProgressOrNew, workToUiWork))
     .then(data => res.render('index', data))
     .catch(e => 'Failed to fetch new Hour: ' + e);
 });
