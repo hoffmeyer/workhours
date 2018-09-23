ec1bc4f08e3d71e17f88a5879779b58d3980c720
Flemming Hoffmeyer <flemming.b.h@gmail.com>
2018-09-23 21:42:12 +0200

    Got the new wepack server working somewhat

---
+++ client/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const outputDir = path.join(__dirname, "build/");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  entry: './src/Index.bs.js',
+  mode: isProd ? 'production' : 'development',
+  output: {
+    path: outputDir,
+    publicPath: outputDir,
+    filename: 'Index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Title from the template',
+      template: 'src/index.html',
+    })
+  ],
+  devServer: {
+    compress: true,
+    port: 3000
+  },
+};
